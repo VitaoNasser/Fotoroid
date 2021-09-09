d21de12d235c3027a3be89a88cfd3a8d34d31518
VitaoNasser <victor.nasser@sga.pucminas.br>
2021-09-09 10:03:36 -0300

    Initial Commit

---
+++ Fotoroid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fotoroid
+//
+//  Created by Rodrigo Dias on 09/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
