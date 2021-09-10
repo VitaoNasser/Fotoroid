0e323c8ca5a65a7bc638240b76399eec471c96c0
VitaoNasser <victor.nasser@sga.pucminas.br>
2021-09-10 01:15:19 -0300

    applying filters

--- Fotoroid/EffectsViewController.swift
+++ Fotoroid/EffectsViewController.swift
@@ -9,15 +9,26 @@ import UIKit
 
 class EffectsViewController: UIViewController {
 
+    
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var viLoading: UIView!
     @IBOutlet weak var ivPhoto: UIImageView!
     
     var image: UIImage!
+    lazy var filterManager: FilterManager = {
+        let filterManager = FilterManager(image: image)
+        return filterManager
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ivPhoto.image = image
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        ivPhoto.image = filterManager.applyFilter(type: FilterType(rawValue: 5)!)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,3 +47,4 @@ class EffectsViewController: UIViewController {
     */
 
 }
+
